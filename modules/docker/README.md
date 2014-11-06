aa88f62663554506519aa6e82819485a5e2e1ae0
Daniel Haus <daniel.haus@businesstools.de>
2014-11-06 09:41:38 +0100

    Move docker settings to submodule

---
+++ modules/docker/README.md
@@ -0,0 +1,5 @@
+Docker
+======
+
+Provides some shortcuts and helpers for docker and especially boot2docker on OS X.
+
---
+++ modules/docker/init.zsh
@@ -0,0 +1,13 @@
+#
+# Provides some shortcuts and helpers for docker
+# and especially boot2docker on OS X
+#
+
+# boot2docker
+if which boot2docker &> /dev/null; then
+  $(boot2docker shellinit 2>/dev/null)
+fi
+
+function docker-ip() {
+  boot2docker ip 2>/dev/null
+}
