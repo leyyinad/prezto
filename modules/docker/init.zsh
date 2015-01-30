e0dfc36c227b38ae54258974cda43bd6a6a56bf0
Daniel Haus <daniel.haus@businesstools.de>
2015-01-30 09:49:26 +0100

    Add docker-enter function

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -6,6 +6,13 @@
 # boot2docker
 if which boot2docker &> /dev/null; then
     $(boot2docker shellinit 2>/dev/null)
+
+    # see
+    # http://blog.sequenceiq.com/blog/2014/07/05/docker-debug-with-nsenter-on-boot2docker/
+    function docker-enter() {
+      boot2docker ssh '[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter'
+      boot2docker ssh -t sudo /var/lib/boot2docker/docker-enter "$@"
+    }
 fi
 
 function docker-ip() {
