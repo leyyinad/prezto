356a3f009c55d6111f1623fc167292c313dce28d
Daniel Haus <daniel.haus@businesstools.de>
2015-11-27 12:57:23 +0100

    Fix typo

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -3,4 +3,4 @@
 #
 alias docker-clean-containers="docker ps -a -notrunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 -r docker rm"
 alias docker-clean-images="docker images -a -notrunc | grep none | awk '{print \$3}' | xargs -L 1 -r docker rmi"
-alias docker-clean="docker-cleancontainers && docker-cleanimages"
+alias docker-clean="docker-clean-containers && docker-clean-images"
