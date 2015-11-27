1c619036d15381ab38591252fd2383b54e351b22
Daniel Haus <daniel.haus@businesstools.de>
2015-11-27 12:59:05 +0100

    Fix options for latest docker and OS X

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -1,6 +1,6 @@
 #
 # Provides some shortcuts and helpers for docker
 #
-alias docker-clean-containers="docker ps -a -notrunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 -r docker rm"
-alias docker-clean-images="docker images -a -notrunc | grep none | awk '{print \$3}' | xargs -L 1 -r docker rmi"
+alias docker-clean-containers="docker ps -a --no-trunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 docker rm"
+alias docker-clean-images="docker images -a --no-trunc | grep none | awk '{print \$3}' | xargs -L 1 docker rmi"
 alias docker-clean="docker-clean-containers && docker-clean-images"
