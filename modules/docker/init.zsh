b42bd259d33f25bff1ac4e93c49781b546da8f9d
Daniel Haus <daniel.haus@businesstools.de>
2015-11-27 12:56:35 +0100

    Remove boot2docker tools, add cleanup commands

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -1,20 +1,6 @@
 #
 # Provides some shortcuts and helpers for docker
-# and especially boot2docker on OS X
 #
-
-# boot2docker
-if which boot2docker &> /dev/null; then
-    $(boot2docker shellinit 2>/dev/null)
-
-    # see
-    # http://blog.sequenceiq.com/blog/2014/07/05/docker-debug-with-nsenter-on-boot2docker/
-    function docker-enter() {
-      boot2docker ssh '[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter'
-      boot2docker ssh -t sudo /var/lib/boot2docker/docker-enter "$@"
-    }
-fi
-
-function docker-ip() {
-  boot2docker ip 2>/dev/null
-}
+alias docker-clean-containers="docker ps -a -notrunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 -r docker rm"
+alias docker-clean-images="docker images -a -notrunc | grep none | awk '{print \$3}' | xargs -L 1 -r docker rmi"
+alias docker-clean="docker-cleancontainers && docker-cleanimages"
