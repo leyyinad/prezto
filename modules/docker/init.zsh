ec7f2542ef4efa13c089ff6388755763047521bc
Daniel Haus <daniel.haus@businesstools.de>
2016-02-19 12:33:34 +0100

    Add aliases

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -4,3 +4,11 @@
 alias docker-clean-containers="docker ps -a --no-trunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 docker rm"
 alias docker-clean-images="docker images -a --no-trunc | grep none | awk '{print \$3}' | xargs -L 1 docker rmi"
 alias docker-clean="docker-clean-containers && docker-clean-images"
+
+alias machine="docker-machine"
+alias compose="docker-compose"
+alias swarm="docker-swarm"
+
+alias d="docker"
+alias dm="docker-machine"
+alias dc="docker-machine"
