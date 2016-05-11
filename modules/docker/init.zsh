68c274f85b4271e11b8cda1775805992e6e1d32a
Daniel Haus <daniel.haus@businesstools.de>
2016-05-11 09:37:51 +0200

    Remove aliases in favor of 'docker-clean' (in homebrew)

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -1,9 +1,6 @@
 #
 # Provides some shortcuts and helpers for docker
 #
-alias docker-clean-containers="docker ps -a --no-trunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 docker rm"
-alias docker-clean-images="docker images -a --no-trunc | grep none | awk '{print \$3}' | xargs -L 1 docker rmi"
-alias docker-clean="docker-clean-containers && docker-clean-images"
 
 alias machine="docker-machine"
 alias compose="docker-compose"
